a15554da76f20f10876233688790fc51e59d27de
totemd <mat.prandi@gmail.com>
2020-11-04 17:36:06 +0100

    Refactor to use classes

--- script.js
+++ script.js
@@ -1,75 +1,111 @@
-let library = [];
+class Library {
+    library = [];
 
-function Book(title, author, numberOfPages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.isRead = isRead;
-}
-
-function addBook(book) {
-    library.push(book);
-    displayLibrary();
-}
-
-function deleteBook(index) {
-    library.splice(index, 1);
-    displayLibrary();
-}
-
-function toggleRead(index) {
-    library[index].isRead = !library[index].isRead
-    displayLibrary();
-}
-
-function displayLibrary() {
-    const tableBody = document.querySelector("tbody");
-    for (let i = tableBody.rows.length-1; i >= 0; i--){
-        tableBody.deleteRow(i);
+    add(book) {
+        this.library.push(book);
+        this.displayLibrary();
     }
-    let index = 0;
-    library.forEach(book => {
-        const row = tableBody.insertRow();
 
-        let cell = row.insertCell();
-        cell.textContent = book.title;
-        cell = row.insertCell();
-        cell.textContent = book.author;
-        cell = row.insertCell();
-        cell.textContent = book.numberOfPages;
+    delete(index) {
+        this.library.splice(index, 1);
+        this.displayLibrary();
+    }
 
-        cell = row.insertCell();
-        const readCheckBox = document.createElement("input");
-        readCheckBox.setAttribute("type", "checkbox");
-        readCheckBox.setAttribute("class", "read-checkbox");
-        readCheckBox.setAttribute("id", `check${index}`);
-        readCheckBox.checked = book.isRead;
-        readCheckBox.addEventListener("change", (e) => {
-            const index = +e.target.id.match(/\d+/);
-            toggleRead(index);
-        })
-        cell.appendChild(readCheckBox);
+    toggleRead(index) {
+        this.library[index].toggleRead();
+        this.displayLibrary();
+    }
 
-        cell = row.insertCell();
-        // const deleteButton = document.createElement("button");
-        // deleteButton.setAttribute("type", "button");
-        cell.setAttribute("class", "delete-button");
-        cell.setAttribute("id", `delete${index++}`);
-        cell.textContent = "Delete";
-        cell.addEventListener("click", (e) => {
-            const index = +e.target.id.match(/\d+/);
-            deleteBook(index);
-        })
-        // cell.appendChild(deleteButton);
-    });
+    displayLibrary() {
+        // Delete library
+        const tableBody = document.querySelector("tbody");
+        for (let i = tableBody.rows.length - 1; i >= 0; i--) {
+            tableBody.deleteRow(i);
+        }
+        // Show library
+        let index = 0;
+        this.library.forEach(book => {
+            const row = tableBody.insertRow();
+
+            let cell = row.insertCell();
+            cell.textContent = book.title;
+            cell = row.insertCell();
+            cell.textContent = book.author;
+            cell = row.insertCell();
+            cell.textContent = book.numberOfPages;
+
+            cell = row.insertCell();
+            const readCheckBox = document.createElement("input");
+            readCheckBox.setAttribute("type", "checkbox");
+            readCheckBox.setAttribute("class", "read-checkbox");
+            readCheckBox.setAttribute("id", `check${index}`);
+            readCheckBox.checked = book.isRead;
+            readCheckBox.addEventListener("change", (e) => {
+                const index = +e.target.id.match(/\d+/);
+                this.toggleRead(index);
+            });
+            cell.appendChild(readCheckBox);
+
+            cell = row.insertCell();
+            cell.setAttribute("class", "delete-button");
+            cell.setAttribute("id", `delete${index++}`);
+            cell.textContent = "Delete";
+            cell.addEventListener("click", (e) => {
+                const index = +e.target.id.match(/\d+/);
+                this.delete(index);
+            });
+        });
+    }
+
+    log() {
+        console.table(this.library);
+    }
 }
 
-library.push(new Book("The Hobbit", "J.R.R. Tolkien", 1442, false));
-library.push(new Book("The Bible", "Unknown", 555, false));
-library.push(new Book("Withering Heights", "Emily Brontë", 168, false));
-library.push(new Book("La Serpe", "Philippe Jaennada", 671, true));
+class Book {
+    title;
+    author;
+    numberOfPages;
+    isRead;
 
-displayLibrary();
+    constructor(title, author, numberOfPages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.isRead = isRead;
+    }
+
+    get title() {
+        return this.title;
+    }
+
+    get author() {
+        return this.author;
+    }
+
+    get numberOfPages() {
+        return this.numberOfPages;
+    }
+
+    get isRead() {
+        return this.isRead;
+    }
+
+    toggleRead() {
+        this.isRead = !this.isRead;
+    }
+
+
+}
+
+let library = new Library();
+
+library.add(new Book("The Hobbit", "J.R.R. Tolkien", 1442, false));
+library.add(new Book("The Bible", "Unknown", 555, false));
+library.add(new Book("Withering Heights", "Emily Brontë", 168, false));
+library.add(new Book("La Serpe", "Philippe Jaennada", 671, true));
+
+library.displayLibrary();
 
 const formPopup = document.querySelector("#form-popup");
 const openFormButton = document.querySelector("#open-form-button");
@@ -87,7 +123,7 @@ closeFormButton.addEventListener("click", () => {
 const form = document.forms[0];
 const submitButton = document.querySelector("#add-button");
 submitButton.addEventListener("click", () => {
-    addBook(new Book(form.title.value, form.author.value, form.pages.value, form.read.checked));
+    library.add(new Book(form.title.value, form.author.value, form.pages.value, form.read.checked));
     form.reset();
     formPopup.style.display = "none";
     openFormButton.style.display = "block";
